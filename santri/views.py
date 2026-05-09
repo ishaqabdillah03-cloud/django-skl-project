f3d52cc85cd6a8e42e83da26639cb48f5679e219
Ishaq Abdillah <ishaqabdillah03@gmail.com>
2026-05-09 21:18:35 +0700

    first commit

---
+++ santri/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def santri_home(request):
+    return render(request, "beranda.html")
+
+
+def biodata(request):
+    return render(request, "biodata.html")
+
+
+def jadwal(request):
+    return render(request, "jadwal.html")
+
+
+def galeri(request):
+    return render(request, "galeri.html")
+
+
+def feedback(request):
+    return render(request, "feedback.html")
